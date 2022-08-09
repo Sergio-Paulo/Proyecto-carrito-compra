9a02079c8959b00a8018491e40a797d1ac6e5ff1
Sergio Torres Fernández <spaulomusic@gmail.com>
2022-08-09 19:52:44 -0300

    leer contenido HTML (info curso)

--- js/app.js
+++ js/app.js
@@ -15,7 +15,28 @@ function cargaEventListeners() {
 
 function agregarCurso(evento) {
     evento.preventDefault();
-    if ( evento.target.classList.contains('agregar-carrito') ){
-        console.log(evento.target)
+
+    if ( evento.target.classList.contains('agregar-carrito') ) {
+        const cursoSeleccionado = evento.target.parentElement.parentElement;
+
+        leerDatosCurso(cursoSeleccionado);
     }
 };
+
+// Lee el contenido HTML al que le damos click y lo usa en el carrito
+
+function leerDatosCurso(curso) {
+    console.log(curso);
+
+    // crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+    
+    console.log(infoCurso);
+
+}
